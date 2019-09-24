0452ea5a03db9e219779b2dbeee2383314ad1954
Scott Ford <scott@scottford.io>
2019-09-24 11:46:10 -0700

    updating plan
    Signed-off-by: Scott Ford <scott@scottford.io>

--- 02_effortless_config/habitat/plan.sh
+++ 02_effortless_config/habitat/plan.sh
@@ -1,19 +1,34 @@
-#@IgnoreInspection BashAddShebang
-if [ -z ${CHEF_POLICYFILE+x} ]; then
-  policy_name="base"
-else
-  policy_name=${CHEF_POLICYFILE}
-fi
-
-scaffold_policy_name="$policy_name"
 pkg_name=config-baseline
-pkg_origin=effortless
+pkg_origin=chef
 pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_description="The Chef $scaffold_policy_name Policy"
+pkg_license=("Apache-2.0")
 pkg_upstream_url="http://chef.io"
-pkg_scaffolding="core/scaffolding-chef"
+pkg_scaffolding="chef/scaffolding-chef-infra"
 pkg_svc_user=("root")
-pkg_exports=(
-  [chef_client_ident]=chef_client_ident
-)
+scaffold_policy_name="base"
+
+
+#######################################
+# Optional settings
+#######################################
+
+# You don't usually need to change these.
+# This project provides highly tuned defaults for you.
+# If you don't have a strong reason for overriding these
+# Then it's a good idea to remove them.
+
+# scaffold_policyfile_path="$PLAN_CONTEXT"
+# allows you to use a policyfile in any location in your repo
+
+# scaffold_chef_client="chef/chef-client"
+# allows you to hard-pin to a version of the chef-infra-client
+
+# scaffold_chef_dk="chef/chef-dk"
+# allows you to hard-pin to a version of chef-dk
+
+# scaffold_data_bags_path="$PLAN_CONTEXT/../data_bags"
+# allows you to build data bags into the package
+
+# scaffold_cacerts="origin/cacerts"
+# allows you to specify a custom cacert package
