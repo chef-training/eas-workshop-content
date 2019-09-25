83cd497d776f23fdf1e8c1b9803837cec308cee2
Scott Ford <scott@scottford.io>
2019-09-24 20:57:54 -0700

    updating origins
    Signed-off-by: Scott Ford <scott@scottford.io>

--- 01_effortless_audit/linux-audit-baseline/habitat/plan.sh
+++ 01_effortless_audit/linux-audit-baseline/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=audit-baseline
 pkg_version=0.1.0
-pkg_origin=chef
+pkg_origin=eas-workshop
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="Effortless Linux Audit Example"
--- 02_effortless_config/habitat/plan.sh
+++ 02_effortless_config/habitat/plan.sh
@@ -1,5 +1,5 @@
 pkg_name=config-baseline
-pkg_origin=chef
+pkg_origin=eas-workshop
 pkg_version="0.1.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
